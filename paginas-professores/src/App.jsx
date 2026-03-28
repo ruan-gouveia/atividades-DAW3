0df032d2954b10138e14ccef1f0f2991d478a855
Ruan Gouveia <ruangouveia78@gmail.com>
2026-03-27 23:40:01 -0300

    corrige capitalização dos componentes

--- paginas-professores/src/App.jsx
+++ paginas-professores/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Menu from "./components/menu";
+import Menu from "./components/Menu";
 import PaginaInicial from "./components/PaginaInicial";
 import PaginaProfessor from "./components/PaginaProfessor";
 
